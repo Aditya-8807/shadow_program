9b751fb7902c408e36cf089d0f27eda1d90c8070
vaibhavsingh777 <vaibhavworkiitb@gmail.com>
2025-08-18 21:45:26 +0530

    past event page and eventcard files fixed for responsiveness

--- frontend/src/Pages/past-events-page/past-events.jsx
+++ frontend/src/Pages/past-events-page/past-events.jsx
@@ -1,134 +1,142 @@
-import { useState } from 'react';
-import EventCard from "../../components/event-card/EventCard"
-import "./past-events.css"
-import Carousel from "../../components/Image-slider/Carousel"
+import { useState } from "react";
+import EventCard from "../../components/event-card/EventCard";
+import "./past-events.css";
+import Carousel from "../../components/Image-slider/Carousel";
 
 function PastEvent() {
-    const [activeYear, setActiveYear] = useState('All');
-    const events = [
-        {
-            id: 1, company: "WorldQuant", date: "Oct,2025",
+  const [activeYear, setActiveYear] = useState("All");
+  const events = [
+    {
+      id: 1,
+      company: "WorldQuant",
+      date: "Oct,2025",
+      photo: "/images/past_event/world_quant.jpg",
+      info: "",
+    },
+    {
+      id: 2,
+      company: "FYND",
+      date: "Oct,2025",
+      photo: "/images/past_event/fynd.jpg",
+      info: "",
+    },
+    {
+      id: 3,
+      company: "Bajaj Auto",
+      date: "Oct,2025",
+      photo: "/images/past_event/bajaj_auto.png",
+      info: "",
+    },
+    {
+      id: 4,
+      company: "Deloitte",
+      date: "Oct,2025",
+      photo: "/images/past_event/deloitte.png",
+      info: "",
+    },
+    {
+      id: 5,
+      company: "SBI",
+      date: "Jan,2024",
+      photo: "/images/past_event/sbi.jpg",
+      info: "",
+    },
+    {
+      id: 6,
+      company: "L&T",
+      date: "Apr,2024",
+      photo: "/images/past_event/lnt.png",
+      info: "",
+    },
+    {
+      id: 7,
+      company: "NSE",
+      date: "Oct,2024",
+      photo: "/images/past_event/nse.png",
+      info: "",
+    },
+    {
+      id: 8,
+      company: "CSDL",
+      date: "Nov,2024",
+      photo: "/images/past_event/cdsl.jpg",
+      info: "",
+    },
+    {
+      id: 9,
+      company: "HUL",
+      date: "Mar,2023",
+      photo: "/images/past_event/hul.png",
+      info: "",
+    },
+    {
+      id: 10,
+      company: "BSE",
+      date: "Oct,2022",
+      photo: "/images/past_event/bse.png",
+      info: "",
+    },
+  ];
 
-            photo: "/images/past_event/world_quant.jpg",
-            info: ""
-        },
-        {
-            id: 2, company: "FYND", date: "Oct,2025",
+  // Filter events by year
+  const filterEventsByYear = (year) => {
+    if (year === "All") {
+      return events;
+    }
+    return events.filter((event) => event.date.includes(year));
+  };
 
-            photo: "/images/past_event/fynd.jpg",
-            info: ""
-        },
-        {
-            id: 3, company: "Bajaj Auto", date: "Oct,2025",
+  // Get unique years from events
+  const getUniqueYears = () => {
+    const years = events.map((event) => event.date.split(",")[1]);
+    return [...new Set(years)].sort((a, b) => b - a);
+  };
 
-            photo: "/images/past_event/bajaj_auto.png",
-            info: ""
-        },
-        {
-            id: 4, company: "Deloitte", date: "Oct,2025",
+  const years = getUniqueYears();
+  const filteredEvents = filterEventsByYear(activeYear);
 
-            photo: "/images/past_event/deloitte.png",
-            info: ""
-        },
-        {
-            id: 5, company: "SBI", date: "Jan,2024",
-
-            photo:"/images/past_event/sbi.jpg",
-            info: ""
-        },
-        {
-            id: 6, company: "L&T", date: "Apr,2024",
-
-            photo: "/images/past_event/lnt.png",
-            info: ""
-        },
-        {
-            id: 7, company: "NSE", date: "Oct,2024",
-
-            photo: "/images/past_event/nse.png",
-            info: ""
-        },
-        {
-            id: 8, company: "CSDL", date: "Nov,2024",
-
-            photo: "/images/past_event/cdsl.jpg",
-            info: ""
-        },
-        {
-            id: 9, company: "HUL", date: "Mar,2023",
-
-            photo: "/images/past_event/hul.png",
-            info: ""
-        },
-        {
-            id: 10, company: "BSE", date: "Oct,2022",
-
-            photo: "/images/past_event/bse.png",
-            info: ""
-        }
-
-
-
-    ]
-
-    // Filter events by year
-    const filterEventsByYear = (year) => {
-        if (year === 'All') {
-            return events;
-        }
-        return events.filter(event => event.date.includes(year));
-    };
-
-    // Get unique years from events
-    const getUniqueYears = () => {
-        const years = events.map(event => event.date.split(',')[1]);
-        return [...new Set(years)].sort((a, b) => b - a);
-    };
-
-    const years = getUniqueYears();
-    const filteredEvents = filterEventsByYear(activeYear);
-
-    return <main>
-        <div className='title_container'>
-            <h1 className='page_title'>Past Shadow Programs</h1>
-
-            <h4 className='page-info'>Take a look at our previous successful shadow programs and their impact</h4>
-        </div>
-        <div className='body'>
-            {/* Year Tabs */}
-            <div className="year-tabs-container">
-                <button
-                    className={`year-tab ${activeYear === 'All' ? 'active' : ''}`}
-                    onClick={() => setActiveYear('All')}
-                >
-                    All
-                </button>
-                {years.map(year => (
-                    <div>
-
-                        <button
-                            key={year}
-                            className={`year-tab ${activeYear === year ? 'active' : ''}`}
-                            onClick={() => setActiveYear(year)}
-                        >
-                            {year}
-                        </button>
-                    </div>
-                ))}
-            </div>
-
-            <div className="Events">
-                <div className="events-grid">
-                    {filteredEvents.map((event) => (
-                        <EventCard event={event} key={event.id} />
-                    ))}
-                </div>
-            </div>
-            <div className='carousel'> <Carousel /></div>
+  return (
+    <main>
+      <div className="title_container">
+        <h1 className="page_title">Past Shadow Programs</h1>
+        <h4 className="page-info">
+          Take a look at our previous successful shadow programs and their
+          impact
+        </h4>
+      </div>
+      <div className="body">
+        {/* Year Tabs */}
+        <div className="year-tabs-container">
+          <button
+            className={`year-tab ${activeYear === "All" ? "active" : ""}`}
+            onClick={() => setActiveYear("All")}
+          >
+            All
+          </button>
+          {years.map((year) => (
+            <button
+              key={year}
+              className={`year-tab ${activeYear === year ? "active" : ""}`}
+              onClick={() => setActiveYear(year)}
+            >
+              {year}
+            </button>
+          ))}
         </div>
 
-    </main >
-
+        <div className="Events">
+          <div className="events-grid">
+            {filteredEvents.map((event) => (
+              <EventCard event={event} key={event.id} />
+            ))}
+          </div>
+        </div>
+        <div className="carousel">
+          <Carousel />
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default PastEvent
+export default PastEvent;
--- frontend/src/components/event-card/EventCard.jsx
+++ frontend/src/components/event-card/EventCard.jsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import "./EventCard.css"
-import { FaCalendarAlt } from 'react-icons/fa';
+import React from "react";
+import "./EventCard.css";
+import { FaCalendarAlt } from "react-icons/fa";
 
 function EventCard({ event }) {
-    return (
-        <div className="event-card">
-
-
-
-            <div className="event-info">
-                <div className="event-poster"> <img src={event.photo} />
-                </div>
-                <div className="info">
-                    <p>{event.info}</p>
-                </div>
-            </div>
-            <div className="event-header">
-                <h3>{event.company}</h3>
-                <p className="date">
-                    <FaCalendarAlt className="icon" /> {event.date}
-                </p>
-            </div>
-
-        </div>
-    );
+  return (
+    <div className="event-card">
+      <div className="event-poster">
+        <img src={event.photo} alt={event.company} />
+      </div>
+      <div className="event-details">
+        <h3 className="company-name">{event.company}</h3>
+        <p className="date">
+          <FaCalendarAlt className="icon" />
+          {event.date}
+        </p>
+        {event.info && (
+          <div className="info">
+            <p>{event.info}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default EventCard;
