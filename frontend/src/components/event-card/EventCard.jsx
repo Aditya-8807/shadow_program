e910e0b3dd884418d49adfb6c9b434dcec146924
vaibhavsingh777 <vaibhavworkiitb@gmail.com>
2025-08-22 23:23:39 +0530

    pastevents page 3d effect added

--- frontend/src/components/event-card/EventCard.jsx
+++ frontend/src/components/event-card/EventCard.jsx
@@ -1,22 +1,37 @@
 import React from "react";
+import TiltedCard from "./tilted";
 import "./EventCard.css";
 import { FaCalendarAlt } from "react-icons/fa";
 
 function EventCard({ event }) {
   return (
-    <div className="event-card">
-      <div className="event-poster">
-        <img src={event.photo} alt={event.company} />
-      </div>
-      <div className="event-details">
-        <div className="details-content">
-          <h3 className="company-name">{event.company}</h3>
-          <p className="date">
-            <FaCalendarAlt className="icon" />
+    <div className="event-card-wrapper">
+      <TiltedCard
+        containerWidth="300px"
+        containerHeight="380px"
+        rotateAmplitude={15}
+        scaleOnHover={1.05}
+        showMobileWarning={false}
+        showTooltip={false}
+      >
+        {/* Square Image */}
+        <div className="event-image-section">
+          <img
+            src={event.photo}
+            alt={event.company}
+            className="event-card-image"
+          />
+        </div>
+
+        {/* Text Section Below */}
+        <div className="event-text-section">
+          <h3 className="event-company-name">{event.company}</h3>
+          <p className="event-date">
+            <FaCalendarAlt className="event-date-icon" />
             {event.date}
           </p>
         </div>
-      </div>
+      </TiltedCard>
     </div>
   );
 }
