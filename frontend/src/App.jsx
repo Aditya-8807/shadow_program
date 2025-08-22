05d83c5e1962af96b0a9853b3af88e6741764960
TarunKadam <tarunkadam0806@gmail.com>
2025-08-23 02:27:15 +0530

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,15 +7,29 @@ import TeamPage from "./Pages/team-page/team"
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/footer';
 
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 export default function App() {
   return (
     <div className="app">
       <Navbar />
+      <ScrollToTop /> 
       <div className="content">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/PastEvent" element={<PastEvent/>} />
-          <Route path="/RegistrationPage" element={<RegistrationPage />} />
+          <Route path="/register" element={<RegistrationPage />} />
           <Route path="/TeamPage" element={<TeamPage />} />
         </Routes>
       </div>
