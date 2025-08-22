9c1e78d58beae745090c85d1741227612ec426bd
Aditya-8807 <adityachaurasiya8807@gmail.com>
2025-08-22 23:42:07 +0530

    navbar

--- frontend/src/components/FAQ/faq.jsx
+++ frontend/src/components/FAQ/faq.jsx
@@ -65,7 +65,7 @@ const FAQ = () => {
   const scrollGap = isMobile ? "1rem" : "1rem";
 
   return (
-    <section className="faq-section">
+    <section id="faq" className="faq-section">
       {/* Animated title */}
       <ScrollVelocity
         texts={scrollTexts}
